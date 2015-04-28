c0c093c4840613da4b502addd94be2740ccae693
Hannes <Hannes@10.29.1.21>
2015-04-28 12:34:30 +0200

    Fix Converter Klasse #Issue 767

--- WOTS-Visualisierung_1/src/files/Converter.java
+++ WOTS-Visualisierung_1/src/files/Converter.java
@@ -1,33 +1,74 @@
 package files;
 
+import javax.xml.bind.DatatypeConverter;
+import org.bouncycastle.util.encoders.Hex;
 
 public class Converter {
 	
+//	public static void main(String[] args) {
+//		
+//		byte[] seed;
+//		wots.WinternitzOTS instance = new wots.WinternitzOTS(42);
+//		files.PseudorandomFunction prf = new files.AESPRF.AES128();
+//		int n = prf.getLength();
+//	    SecureRandom sRandom = new SecureRandom();
+//	    seed = new byte[n];
+//	    
+//	    sRandom.nextBytes(seed);
+//
+//	    byte[] x = new byte[n];
+//		
+//	    sRandom.nextBytes(x);
+//	    instance.init(prf, x);
+//	    
+//	    instance.generatePrivateKey(seed);
+//	    
+//	    byte[][] key = instance.getPrivateKey();
+//	    
+//	    System.out.println(_2dByteToHex(key));
+//	    System.out.println(_byteToHex(key[0]));
+//	    System.out.println("##############################");
+//	    //System.out.println(key[0]);
+//	    String output = _2dByteToHex(key);
+//	    //System.out.println(output);
+//	    
+//	    byte[][] output2 = _stringTo2dByte(output, instance.getLength());
+//	    
+//	    System.out.println(_2dByteToHex(output2));
+//	    
+//	    System.out.println(_byteToHex(output2[0]));
+//		
+//	}
+	
 	public String _2dByteToHex (byte[][] input) {
 		
-		String output = null;
+		String output = "";
+		
+		for (int i = 0; i < input.length; i++) {
+	    	output += Hex.toHexString(input[i]);
+	    }
 		
 		return output;
 	}
 	
 	public String _byteToHex (byte[] input) {
-		
-		String output = null;
-		
-		return output;
+		return Hex.toHexString(input);
 	}
 	
-	public byte[][] _stringTo2dByte (String input) {
+	public byte[][] _stringTo2dByte (String input, int l) {
 		
-		byte[][] output = null;
+		byte[][] output = new byte[l][];
+		
+		int s = input.length()/l;
+		
+		for (int i = 0; i < l; i++) {
+			output[i] = _stringToByte(input.substring(i*s,s+i*s));
+		}
 		
 		return output;
 	}
 	
 	public byte[] _stringToByte (String input) {
-		
-		byte[] output = null;
-		
-		return output;
+		return DatatypeConverter.parseHexBinary(input);
 	}
 }
